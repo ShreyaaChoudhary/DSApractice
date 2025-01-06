66e05d862e804df9b273ae7185579499b0a88a2f
12208155 <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-01-06 20:02:46 +0530

    Time: 1058 ms (5.16%) | Memory: 24.4 MB (5.76%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int flag;
         map<int,int> mp;
-        for(int i = 0 ; i< nums.size(); i++){
-            mp[nums[i]] = count(nums.begin(), nums.end(),nums[i]); 
+        int flag;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=count(nums.begin(),nums.end(),nums[i]);
         }
-        for(auto &it: mp){
+        for(auto& it: mp){
             if(it.second==1){
                 flag = it.first;
             }
