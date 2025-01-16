ef4f84058e73b9fd9f4e19d2c80e2460eeae1819
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-01-16 09:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (86.04%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        //dutch national flag algorithm --optimal
+        //0 - low-1 -> 0
+        //low - mid-1 ->1
+        //mid - high-1 -> random nos
+        //high - n-1 -> 2
+        int n = nums.size();
+        int mid = 0, low = 0 , high = n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
