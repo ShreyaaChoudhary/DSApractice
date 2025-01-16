3f422a0d342319d9da5dbaa6ecf4d122b63243fd
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-01-16 10:32:09 +0530

    Time: 0 ms (100.00%) | Memory: 50.8 MB (25.78%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-         vector<int> max;
-        int c = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
-                c++;  
-            } else {
-                max.push_back(c);  
-                c = 0;  
-            }
-        }
-        
-        max.push_back(c);
+    int findMaxConsecutiveOnes(vector<int>& nums) 
+    {
 
-        auto max_elem = max_element(max.begin(), max.end());
-        return *max_elem;  
+    vector<int> max;
+    int count = 0 ;
+    for(int i= 0 ; i<nums.size(); i++){
+        if(nums[i]==1){
+            count++;
+        }
+        else{
+            max.push_back(count);
+            count=0;
+        }
     }
+    max.push_back(count);
+    auto ans = max_element(max.begin(),max.end());
+    return *ans;
+
+    }
+
+
+
+
+
+
+
+
+
+    //      vector<int> max;
+    //     int c = 0;
+        
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         if (nums[i] == 1) {
+    //             c++;  
+    //         } else {
+    //             max.push_back(c);  
+    //             c = 0;  
+    //         }
+    //     }
+        
+    //     max.push_back(c);
+
+    //     auto max_elem = max_element(max.begin(), max.end());
+    //     return *max_elem;  
+    // }
 };
