d7e1b50976f965e09f637edfe7565cbb14732c46
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-01-27 09:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (33.09%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int ind = -1 ;
-        int n = nums.size();
-        //longest prefix match
-        for(int i = n-2; i>=0; i--){
+        int ind = -1;
+        int n= nums.size();
+        //1st step
+        for(int i = n-2; i>=0; i-- ){
             if(nums[i]<nums[i+1]){
                 ind = i;
                 break;
             }
-        } 
-        //no dip
-        if(ind == -1){
+        }
+        if(ind==-1){
+            //no dip
             reverse(nums.begin(), nums.end());
             return;
         }
-        //2
+        //2nd step
         for(int i = n-1; i>=ind; i--){
             if(nums[i]>nums[ind]){
                 swap(nums[i],nums[ind]);
                 break;
             }
         }
-        reverse(nums.begin()+ind+1,nums.end());
+        reverse(nums.begin()+ind+1, nums.end());
     }
 };
