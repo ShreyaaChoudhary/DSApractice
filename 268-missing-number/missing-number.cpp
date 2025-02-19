720eb54c7d89a5e9439e83d6e801e5f874305809
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-02-19 10:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (39.00%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -12,12 +12,22 @@ public:
         // }
         // return 0;
         //second way
+        // int n= nums.size();
+        // int sum = (n*(n+1))/2;
+        // int nosum = 0 ;
+        // for(int i= 0; i<nums.size(); i++){
+        //     nosum+=nums[i];
+        // }
+        // return sum-nosum;
+        //third way 
         int n= nums.size();
-        int sum = (n*(n+1))/2;
-        int nosum = 0 ;
-        for(int i= 0; i<nums.size(); i++){
-            nosum+=nums[i];
+        int temp = 0;
+        for(int i = 0  ; i<=n; i++){
+            temp^=i;
         }
-        return sum-nosum;
+        for(int i = 0 ; i<n ; i++){
+            temp^=nums[i];
+        }
+        return temp;
     }
 };
