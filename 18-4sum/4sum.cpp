99e0d03505a07e05b26d5bb22185193466034999
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-02-27 15:08:05 +0530

    Time: 23 ms (56.72%) | Memory: 17.4 MB (59.96%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,39 +4,36 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            for (int j = i + 1; j < n; j++) {
-                if (j > i+1 && nums[j] == nums[j - 1])
-                    continue;
-                int k = j + 1;
-                int l = n - 1;
-
-                while (k < l) {
+        for(int i = 0; i< n; i++){
+            //repeated
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j = i+1; j< n; j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                //2 fixed 2 moving pointers 
+                int p = j+1;
+                int q = n-1;
+                while(p<q){
                     long long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if (sum == target) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    sum+=nums[j];
+                    sum+=nums[p]+nums[q];
+                    if(sum==target){
+                        vector<int> temp = {nums[i], nums[j], nums[p], nums[q]};
                         ans.push_back(temp);
-                        k++;
-                        l--;
-                        while (k < l && nums[k] == nums[k - 1])
-                            k++;
-                        while (k < l && nums[l] == nums[l + 1])
-                            l--;
+                        p++;
+                        q--;
+                        //repeated
+                        while(p<q && nums[p]==nums[p-1]) p++;
+                        while(p<q && nums[q]==nums[q+1]) q--;
+
                     }
                     else if(sum<target){
-                        k++;
+                        p++;
                     }
                     else{
-                        l--;
+                        q--;
                     }
                 }
             }
-        }
-        return ans;
+        } return ans;
     }
 };
