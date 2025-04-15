223e978d3bb12bb21a0c7f0f5333e75e45009cb4
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-04-15 21:36:14 +0530

    Time: 3 ms (8.71%) | Memory: 15.2 MB (19.73%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -6,31 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        if (!root)
-            return result;
+        if(root==nullptr) return result;
         queue<TreeNode*> q;
         q.push(root);
         bool flag = true;
-        while (!q.empty()) {
+        while(!q.empty()){
             int n = q.size();
             vector<int> row(n);
-            for (int i = 0; i < n; i++) {
-                TreeNode* curr = q.front();
+            for(int i = 0 ; i< n ; i++){
+                TreeNode* curr  = q.front();
                 q.pop();
-                int index = flag ? i : n - i - 1;
-                row[index] = curr->val;
-                if (curr->left) {
-                    q.push(curr->left);
-                }
-                if (curr->right) {
+                int index = flag?i:n-i-1;
+                row[index] = curr->val; 
+                if(curr->left)
+                q.push(curr->left);
+                if(curr->right){
                     q.push(curr->right);
                 }
             }
@@ -38,5 +35,6 @@ public:
             result.push_back(row);
         }
         return result;
+
     }
 };
