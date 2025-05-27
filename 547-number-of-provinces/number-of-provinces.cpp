2f59e753916e2d63197e3f9ea27463559484ceb7
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-05-27 17:15:42 +0530

    Time: 11 ms (8.14%) | Memory: 20.9 MB (22.78%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,38 +1,33 @@
 class Solution {
 private:
-    void dfs_traversal(int node, vector<vector<int>> &adjList, vector<int> &vis){
-        vis[node] = 1;
-        for(int it: adjList[node]){
-            if(!vis[it]){
-                dfs_traversal(it, adjList, vis);
+    void dfsofgraph(vector<int> &vis,vector<int> adj[], int node){
+        vis[node]=1;
+        for(auto m:adj[node]){
+            if(!vis[m]){
+                dfsofgraph(vis,adj,m);
             }
         }
     }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        //Convert adj matrix to adj list (we are tring to solve the problems in adj list)
         int n = isConnected.size();
-        vector<vector<int>> adjList(n);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                //check for connections and self loops
-                if (isConnected[i][j] == 1 && i!=j){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i); 
-                }   
+        vector<int> adjlist[n];
+        for(int i = 0 ; i< n ; i++){
+            for(int j = 0 ; j<n; j++)
+            {
+                if(isConnected[i][j]==1 && i!=j){
+                adjlist[i].push_back(j);
+                adjlist[j].push_back(i);
+            }}
+        }
+        vector<int> vis(n);
+        int c =0;
+        for(int i = 0 ; i< n ; i++){
+            if(vis[i]==0){
+                c++;
+                dfsofgraph(vis, adjlist, i);
             }
         }
-        //Variables
-        vector<int> vis(n,0);
-        int count = 0;
-
-        //Updating the count variable and calling the function
-        for(int i=0; i<n; i++){
-            if(vis[i] == 0){
-                count++;
-                dfs_traversal(i, adjList, vis);
-            }
-        }
-        return count;
+        return c;
     }
 };
