d5e2d1194afb840a7450604c4b9e6808fc783503
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-05-28 21:37:00 +0530

    Time: 34 ms (77.93%) | Memory: 18.5 MB (53.61%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,14 +9,42 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        while(headB!=NULL){
-            ListNode* temp = headA;
-            while(temp){
-                if(temp==headB) return headB;
-                temp = temp->next;
-            }
-            headB= headB->next;
-        }   
-        return NULL; 
+        //APPROACH 1
+        // while(headB!=NULL){
+        //     ListNode* temp = headA;
+        //     while(temp){
+        //         if(temp==headB) return headB;
+        //         temp = temp->next;
+        //     }
+        //     headB= headB->next;
+        // }   
+        // return NULL;
+
+        //APPROACH 2
+        //using hashing
+        // unordered_set<ListNode*> st;
+        // while(headA){
+        //     st.insert(headA);
+        //     headA= headA->next;
+        // } 
+        // while(headB){
+        //     if(st.find(headB)!=st.end()) return headB;
+        //     headB = headB->next;
+        // }
+        // return NULL;
+
+
+
+        //optimised
+
+        ListNode* d1 = headA;
+    ListNode* d2 = headB ;
+    
+    while(d1 != d2) {
+        d1 = d1 == NULL? headB:d1->next;
+        d2 = d2 == NULL? headA:d2->next;
+    }
+    
+    return d1;
     }
 };
