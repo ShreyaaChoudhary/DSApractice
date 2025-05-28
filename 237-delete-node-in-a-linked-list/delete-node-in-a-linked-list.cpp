937b2029da983586a708c87f410fd646f9cd5c19
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-05-28 18:26:09 +0530

    Time: 7 ms (52.20%) | Memory: 12.4 MB (18.11%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,10 +9,8 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        //4 5 1 9 
-        //5 
-        ListNode* temp = node->next; //ye store krega ka 1
-        node->val = temp->val;// 5->data = 1->data to it will be 1 ka data
-        node->next = temp->next; //1->data points to 9 
-        }
+        ListNode* temp = node->next;
+        node->val = temp->val;
+        node->next = temp->next;
+    }
 };
