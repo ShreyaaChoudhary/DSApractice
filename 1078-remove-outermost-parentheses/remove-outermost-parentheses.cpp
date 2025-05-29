efb5199c3ada192039b5e3a68a9f3089b47c6148
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-05-29 18:07:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.44%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int count = 0;
+        //depth counter method
+        // int count = 0;
+        // string res = "";
+        // int i = 0;
+        // while(i<s.size()){
+        //     if(s[i]=='('){
+        //         if(count>0){
+        //             res+=s[i];
+        //         }
+        //         count++;
+        //     }
+        //     else{
+        //         count--;
+        //         if(count>0){
+        //             res+=s[i];
+        //         }
+        //     }
+        //     i++;
+        // }
+        // return res;
+
+        //using stack
+        stack<char> st;
+        if(s.empty()) return "";
         string res = "";
-        int i = 0;
-        while(i<s.size()){
+        for(int i = 0; i< s.length(); i++){
+            if(st.empty()){
+                st.push(s[i]);
+                continue; //skips all other iterations
+            }
             if(s[i]=='('){
-                if(count>0){
-                    res+=s[i];
-                }
-                count++;
+                st.push('(');
             }
-            else{
-                count--;
-                if(count>0){
-                    res+=s[i];
-                }
+            if(s[i]==')'){
+                st.pop();
             }
-            i++;
+            if(!st.empty()){
+                res+=s[i];
+            }
         }
         return res;
     }
