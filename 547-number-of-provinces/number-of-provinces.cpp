dabad3270a386937744997ce9f4b7d3dd9353124
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-06-09 15:11:47 +0530

    Time: 70 ms (5.14%) | Memory: 64.9 MB (5.02%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,31 +1,33 @@
 class Solution {
 private:
-    void dfsofgraph(vector<int> &vis,vector<int> adj[], int node){
-        vis[node]=1;
-        for(auto m:adj[node]){
-            if(!vis[m]){
-                dfsofgraph(vis,adj,m);
+    void dfs(int start, vector<int> &vis,vector<vector<int>> adjlist){
+        vis[start]=1;
+        for(auto m:adjlist[start]){
+            if(vis[m]==0){
+                dfs(m,vis,adjlist);
             }
         }
     }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> adjlist[n];
-        for(int i = 0 ; i< n ; i++){
-            for(int j = 0 ; j<n; j++)
-            {
-                if(isConnected[i][j]==1 && i!=j){
-                adjlist[i].push_back(j);
-                adjlist[j].push_back(i);
-            }}
+        int n = isConnected.size(); //n+1 size
+        // vector<int> adjlist[n];
+        vector<vector<int>> adjlist(n);
+        for(int i = 0 ; i<n; i++){
+            for(int j = 0 ; j<n; j++){
+                if(isConnected[i][j]==1 && i!=j)//no self loops
+                {
+                    adjlist[i].push_back(j);
+                    adjlist[j].push_back(i);
+                }
+            }
         }
         vector<int> vis(n);
-        int c =0;
-        for(int i = 0 ; i< n ; i++){
+        int c = 0;
+        for(int i = 0; i<n; i++){
             if(vis[i]==0){
                 c++;
-                dfsofgraph(vis, adjlist, i);
+                dfs(i, vis, adjlist);
             }
         }
         return c;
