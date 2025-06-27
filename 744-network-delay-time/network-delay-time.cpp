0108926853ab158f1da7faa929cffce6a685fbdc
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-06-27 13:16:40 +0530

    Time: 89 ms (44.64%) | Memory: 47.8 MB (40.28%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> maxtime(n + 1, INT_MAX); // 1-based indexing
-        vector<pair<int, int>> adj[n + 1];
+        vector<int> maxtime(n+1, INT_MAX);
+        vector<pair<int, int>> adj[n+1];
 
-        for (auto& time : times) {
+        for (auto time : times) {
             int u = time[0], v = time[1], w = time[2];
             adj[u].push_back({v, w});
         }
-
+        maxtime[k]=0;
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
         pq.push({0, k}); // {time, node}
 
         while (!pq.empty()) {
             auto [time, node] = pq.top();
             pq.pop();
-
-            if (maxtime[node] != INT_MAX) continue; 
-            maxtime[node] = time; 
-
+            if (time > maxtime[node]) continue;  // this is the valid check
             for (auto [v, t] : adj[node]) {
-                if (maxtime[v] == INT_MAX) {
-                    pq.push({time + t, v}); 
+                if (maxtime[v] > maxtime[node]+t) {
+                    maxtime[v] = maxtime[node]+t;
+                    pq.push({maxtime[v], v}); 
                 }
             }
         }
 
-        int maxi = *max_element(maxtime.begin() + 1, maxtime.end());
+        int maxi = *max_element(maxtime.begin()+1, maxtime.end());
         return (maxi == INT_MAX) ? -1 : maxi;
     }
 };
