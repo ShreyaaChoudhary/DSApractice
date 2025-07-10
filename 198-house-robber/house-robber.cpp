f29ec795649b16d10c71382537d833a166d9015c
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-07-10 10:31:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (99.88%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,15 @@
-#include <vector>
-#include <cstring>
-using namespace std;
-
 class Solution {
 public:
-    int dp[100]; 
-
-    int f(int i , vector<int>& nums) {
-        if (i < 0) return 0;
-        if (dp[i] != -1) return dp[i];
-        
-        int pick = nums[i] + f(i - 2, nums);
-        int nonpick = f(i - 1, nums);
-        return dp[i] = max(pick, nonpick);
+    int dp[100];
+    int f(int i , vector<int> &nums){
+        if(i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int pick = nums[i]+f(i-2, nums);
+        int notpick = f(i-1, nums);
+        return dp[i] = max(pick, notpick);
     }
-
     int rob(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return f(nums.size() - 1, nums);        
+        memset(dp,-1,sizeof(dp));
+        return f(nums.size()-1, nums);
     }
 };
