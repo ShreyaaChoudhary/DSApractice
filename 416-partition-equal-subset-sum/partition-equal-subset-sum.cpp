4aa98952e1ea146fcef5f98e8930240d77eda149
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-07-21 12:20:04 +0530

    Time: 43 ms (94.68%) | Memory: 13.2 MB (98.25%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    // bool findsub(int i, int target , vector<vector<int>> &dp, vector<int> &nums){
+    //     if(target==0) return true;
+    //     if(i==0) return target==nums[i];
+    //     int notpick = findsub(i-1, target, dp, nums); 
+    //     int pick = false;
+    //     if(nums[i]<=target){
+    //         pick = findsub(i-1,target-nums[i], dp, nums);
+    //     }
+    //     return dp[i][target] = notpick||pick;
+    // }
     bool canPartition(vector<int>& nums) {
         int total = accumulate(nums.begin(),nums.end(),0);
         if(total%2!=0) return false;
@@ -14,5 +24,10 @@ public:
             }
         }
         return false;
+        // int total = accumulate(nums.begin(), nums.end(), 0);
+        // if(total%2!=0) return false;
+        // int target = total/2;
+        // vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
+        // return findsub(nums.size()-1,target,dp,nums);
     }
 };
