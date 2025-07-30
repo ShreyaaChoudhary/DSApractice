2db1b96082bde43fc4d9269742bd78c35dfa2a9f
Shreya Choudhary <118660222+ShreyaaChoudhary@users.noreply.github.com>
2025-07-30 20:36:33 +0530

    Time: 23 ms (44.80%) | Memory: 46.4 MB (9.32%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        vector<unsigned long long> dp(amount+1,0);
-        dp[0] = 1;
-        for(auto coin:coins){
-            for(int i = coin ; i<= amount ; i++){
-                dp[i] = (long long)dp[i]+dp[i-coin];
-            }
+    long long calcoin(int i , int target , vector<int> &coins, vector<vector<long long>> &dp){
+        if(i==0){
+            return (target%coins[i]==0);
         }
-        return dp[amount];
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notpick = 0+calcoin(i-1, target, coins, dp);
+        int pick = 0;
+        if(coins[i]<=target){
+            // pick = 1+calcoin(i-1,target-coins[i],coins,dp); this is wrong bcoz we can choose the same coin multipkel times 
+            pick = calcoin(i,target-coins[i],coins,dp);
+
+        }
+        return dp[i][target] = pick+notpick;
+    }
+    int change( int amount,vector<int>& coins) {
+        vector<vector<long long>> dp(coins.size(), vector<long long>(amount+1, -1));
+        int n = coins.size();
+        int ans = calcoin(n-1,amount,coins,dp);
+        return ans;
     }
 };
